3ccd381e21c6dcf94660749179adf5a274df2d46
Xuhao Luo <rossihox@gmail.com>
2023-01-25 12:47:52 -0600

    update mlnx_ofed version

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 cd /opt
 
-sudo wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.3-1.0.0.1/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu18.04-x86_64.tgz
+sudo wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.4-3.6.8.1/MLNX_OFED_LINUX-5.4-3.6.8.1-ubuntu20.04-x86_64.tgz
 
-sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu18.04-x86_64.tgz
-cd MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu18.04-x86_64
+sudo tar -xzvf MLNX_OFED_LINUX-5.4-3.6.8.1-ubuntu20.04-x86_64.tgz
+cd MLNX_OFED_LINUX-5.4-3.6.8.1-ubuntu20.04-x86_64
 sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update
+# The first time of install will stuck at "Installing srptools-54mlnx1...".
+# Restart the machine and reinstall then it will go through 
 
 sudo /etc/init.d/openibd restart
 
